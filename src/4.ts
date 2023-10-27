a25c041424a20852a51dfdd383c1caa919d9b416
Zelenro <125198729+Zelenro@users.noreply.github.com>
2023-10-28 00:30:54 +0300

    toCheck

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,64 @@
+class Key {
+  private signature: number;
+
+  constructor() {
+    this.signature = Math.random();
+    }
+    
+    getSignature(): number {
+        return this.signature;
+    }
+
+  }
+
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected  door: boolean = false;
+  protected  key: Key;
+  private tenants: Person[] = [];
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+      console.log(`${person.getKey().getSignature()} entered the house.`);
+    } else {
+      console.log('The door is closed. Cannot come in.');
+    }
+  }
+
+  abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  openDoor(key: Key): void {
+    if (key.getSignature() === this.key.getSignature()) {
+      this.door = true;
+      console.log('The door is open.');
+    } else {
+      console.log('Invalid key. Cannot open the door.');
+    }
+  }
+}
+
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
